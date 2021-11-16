46d7aec6775779672d30dd8fdc441abbaeb87bf9
alex-bartleynees <alexbartleynees@gmail.com>
2021-11-16 21:00:59 +1300

    Add angular material + post-lists

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,17 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent implements OnInit {
+  enteredValue = '';
+  newPost = 'No Content';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onAddPost() {
-    alert('Post Added!')
+    this.newPost = this.enteredValue;
   }
-
 }
